72555ed1e2a092495ba442f79fb299b969cd59f5
yanxiyaoyao <134763362+yanxiyaoyao@users.noreply.github.com>
2026-02-02 21:09:55 +0800

    Change app.run to use environment-defined port

--- backend/app.py
+++ backend/app.py
@@ -93,4 +93,5 @@ def _make_shell_context():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
